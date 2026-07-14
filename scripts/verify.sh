07452091a32e6e36ffb38e44f07a626e4f83c394
salty2011 <salty2011@proton.me>
2026-07-15 00:55:00 +1000

    feat: bake default quasar user into base image

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -9,6 +9,10 @@ grep -q '^FROM registry.fedoraproject.org/fedora@sha256:' images/quasar-base/Doc
 ./scripts/build.sh quasar-base
 labels="$(docker image inspect quasar-base:dev --format '{{json .Config.Labels}}')"
 jq -e '.["org.quasar.image.contract"] == "1" and .["org.quasar.image.acceleration"] == "optional"' <<<"$labels" >/dev/null
+docker run --rm --entrypoint /bin/bash quasar-base:dev -lc '
+  getent passwd quasar | grep -q "^quasar:x:1000:1000:"
+  getent group quasar | grep -q "^quasar:x:1000:"
+'
 
 result="$(docker run --rm -e PUID=1234 -e PGID=2345 -e HOME=/home/tester -e UMASK=027 quasar-base:dev /bin/sh -c 'id -u; id -g; printf "%s %s %s\\n" "$HOME" "$XDG_CONFIG_HOME" "$XDG_RUNTIME_DIR"; stat -c %a "$HOME"')"
 [[ "$result" == $'1234\n2345\n/home/tester /home/tester/.config /tmp/quasar-runtime-1234\n750' ]]
